0b0870b533039fbabb6590f3e9cd4fd8dc79ad9a
Shahan Neda <shahan.neda@gmail.com>
2020-05-19 23:45:38 -0700

    enabled form

--- js/grayscale.js
+++ js/grayscale.js
@@ -53,17 +53,17 @@
       // }
       // else
       // {
-          // AJAX Code To Submit Form.
-//           $.ajax({
-//               crossDomain: true,
-//               dataType: 'jsonp',
-//               url: "https://leanqualityacademy.com/GilanBuilding/Mail/gilanform.php",
-//               data: dataString,
-//               cache: false,
-//               success: function(result){
-//                 // $(window).scrollTop(0);
+//           AJAX Code To Submit Form.
+          $.ajax({
+              crossDomain: true,
+              dataType: 'jsonp',
+              url: "https://leanqualityacademy.com/GilanBuilding/Mail/gilanform.php",
+              data: dataString,
+              cache: false,
+              success: function(result){
+                // $(window).scrollTop(0);
 
-//               }
+              }
          });
            alert("Your message has been sent.");
       // }
